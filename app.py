64280d143409490b1fca61d52766a1b247dc4148
Sjoerd Haasl <sjoerdhaasl@gmail.com>
2025-05-03 03:53:15 +0200

    Stockholm time

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+
 from flask import Flask, jsonify
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 app = Flask(__name__)
 
 @app.route("/")
-def get_time():
-    now = datetime.utcnow()
+def time():
+    now = datetime.now(ZoneInfo("Europe/Stockholm"))
     return jsonify({
-        "datetime": now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "datetime": now.strftime("%Y-%m-%dT%H:%M:%S%z"),
+        "timezone": "Stockholm, Sweden",
         "unixtime": int(now.timestamp())
     })
 
-if __name__ == "__main__":
-    app.run()
+app.run(host="0.0.0.0", port=81)
